0a54cb13ff4f615f55bc7d0ec44caf7495c5c301
wzd <wzdxx1314@163.com>
2019-07-11 15:58:20 +0800

    登陆页

---
+++ src/views/login/login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">login</div>
+</template>
+<script>
+export default {
+    name:'login'
+}
+</script>
+<style lang="less" scoped>
+.container{
+    width:100%;
+    height:100%;
+    background-color: aqua;
+}
+</style>
+
+
